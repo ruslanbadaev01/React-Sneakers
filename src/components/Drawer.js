533eae1f59f492f56085fe79b374556e06342e0a
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2022-10-23 16:44:42 +0300

    Added Cart

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -5,22 +5,19 @@ function Drawer({onClose,items = []}){
                 <h2 className="mb-30 d-flex justify-between m-30">Корзина 
                     <img onClick={onClose} className="removeBtn  cu-p" src="/img/btn-remove.svg" alt=""/>
                 </h2>
-        
                 <div className="items" style={{flex:1}}>
-
                     {
-                        items.map((obj) =>{
+                        items.map((obj) => (
                             <div className="cartItem d-flex align center mb-20">
-                        <div style={{ backgroundImage: `url${obj.imageUrl}` }} className="cartItemImg"></div>
-                        <div className="mr-20 flex">
-                            <p className="mb-5">{obj.title}</p>
-                            <b>{obj.price} руб.</b>
-                        </div>
-                        <img className="removeBtn" src="/img/btn-remove.svg" alt=""/>
-                    </div>
-                        })
+                                <div style={{ backgroundImage: `url(${obj.imageUrl})` }} className="cartItemImg"></div>
+                                <div className="mr-20 flex">
+                                    <p className="mb-5">{obj.title}</p>
+                                    <b>{obj.price} руб.</b>
+                                </div>
+                                <img className="removeBtn" src="/img/btn-remove.svg" alt=""/>
+                            </div>
+                        ))
                     }
-
                 </div>
                 <div className="cartTotalBlock">
                 <ul >
